d55f78fe5bc725677f6d9ee865f33e80e893cc28
Kisei Tanaka <1595661320@mil>
2021-05-14 16:12:09 -1000

    ok

--- scripts/Test_multiple_surveys.R
+++ scripts/Test_multiple_surveys.R
@@ -4,6 +4,7 @@
 
 library(SimSurvey)
 library(dplyr)
+library(sp)
 
 rm(list = ls())
 
@@ -16,8 +17,8 @@ options(digits = 2)
 
 set.seed(438)
 
-plot(survey_grid)
-plot(survey_grid_kt)
+plot(survey_grid, pch = ".")
+plot(survey_grid_kt, pch = ".")
 
 # base populations with MHI survey grids
 pop = sim_abundance() %>% 
@@ -26,7 +27,7 @@ pop = sim_abundance() %>%
 # Set-up a series of surveys from all combinations of settings supplied
 surveys = expand_surveys(
   
-  set_den = c(1, 2) / 1000,
+  set_den = c(1, 100, 500) / 1000,
   lengths_cap = c(100, 500),
   ages_cap = c(5, 20)
   
@@ -36,12 +37,21 @@ surveys = expand_surveys(
 ## population, and then runs a stratified analysis and compares true vs
 ## estimated values. It may take a while to run.
 
-tests = test_surveys(pop, surveys = surveys, n_sims = 1, n_loops = 10, cores = 8)
+tests = test_surveys(pop, surveys = surveys, n_sims = 1, n_loops = 2, cores = 8)
 doParallel::stopImplicitCluster()
 
 plot_total_strat_fan(tests)
-plot_length_strat_fan(tests, years = 1:5, lengths = 1:100)
+plot_length_strat_fan(tests, surveys = 1:8)
+plot_length_strat_fan(tests,years = 1:3, lengths = 20:40)
 plot_survey_rank(tests)
+plot_survey_rank(tests, which_strat = "length")
+plot_survey_rank(tests, which_strat = "age")
+
+plot_age_strat_fan(tests, surveys = 1:8)
+plot_age_strat_fan(tests, surveys = 1:8, select_by = "age")
+
+plot_error_surface(tests, plot_by = "rule")
+plot_error_surface(tests, plot_by = "samples")
 plot_error_surface(tests, plot_by = "rule")
 
 library(plotly)
@@ -54,14 +64,5 @@ tests$total_strat_error %>%
   layout(xaxis = list(title = "Year"),
          yaxis = list(title = "Abundance index"))
 
-plot_total_strat_fan(tests, surveys = 1:8)
-plot_length_strat_fan(tests, surveys = 1:8)
-plot_age_strat_fan(tests, surveys = 1:8)
-plot_age_strat_fan(tests, surveys = 1:8, select_by = "age")
 
-plot_error_surface(tests, plot_by = "rule")
-plot_error_surface(tests, plot_by = "samples")
-
-plot_survey_rank(tests, which_strat = "length")
-plot_survey_rank(tests, which_strat = "age")
 
