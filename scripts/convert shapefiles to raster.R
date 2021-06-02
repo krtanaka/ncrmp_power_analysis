fe3f254c6e9930790afe3387a05d6cc186870cfa
Kisei Tanaka <1595661320@mil>
2021-06-01 23:25:35 -1000

    finally ok

--- scripts/convert shapefiles to raster.R
+++ scripts/convert shapefiles to raster.R
@@ -5,33 +5,33 @@ library(pbapply)
 
 rm(list = ls())
 
-shp_list = list.files(path = "G:/GIS/hardsoft/MHI/", pattern = "shp.shp")
+shp_list = list.files(path = "G:/GIS/hardsoft/MHI/", pattern = "shp.shp"); shp_list
 
 for (shp_i in 1:length(shp_list)) {
   
-  # shp_i = 1
-
-  # Import shapefile
-  sp_df <- readOGR(paste0("G:/GIS/hardsoft/MHI/", shp_list[shp_i]))[4]
-
-  # first separate hard and soft substrate...
-  sp_df_h <- sp_df[sp_df$HardSoft %in% c("Hard"),]
-  sp_df_s <- sp_df[sp_df$HardSoft %in% c("Soft"),]
+  # shp_i = 8
   
-  if (length(sp_df_s) == 0) sp_df_s = sp_df_h
+  # Import shapefile
+  df <- readOGR(paste0("G:/GIS/hardsoft/MHI/", shp_list[shp_i]))[4]
+  df@data
+  table = data.frame(df@data, i = 1:length(df))
+  hard_i = unique(table[table$HardSoft == "Hard",]$i)
+  soft_i = unique(table[table$HardSoft == "Soft",]$i)
+  land_i = unique(table[table$HardSoft == "Land",]$i)
+  ukwn_i = unique(table[table$HardSoft == "Unknown",]$i)
   
   # Raster template 
-  r <- raster(extent(sp_df))
-  projection(r) <- proj4string(sp_df)
-  res(r) <- 100 # spatial resolution in m
+  r <- raster(extent(df))
+  projection(r) <- proj4string(df)
+  res(r) <- 500 # spatial resolution in m
   
   # Per pixel, identify ID covering largest area
-  r_val_h <-  pbsapply(1:ncell(r), function(i) {
+  r_val <-  pbsapply(1:ncell(r), function(i) {
     
     r_dupl <- r
     r_dupl[i] <- 1
     p <- rasterToPolygons(r_dupl) # Current cell -> polygon
-    sp_df_crp <- crop(sp_df_h, p)   # Crop initial polygons by current cell extent
+    sp_df_crp <- crop(df, p)   # Crop initial polygons by current cell extent
     
     # Case 1: no polygon intersecting current cell
     if (is.null(sp_df_crp)) {                   
@@ -52,52 +52,19 @@ for (shp_i in 1:length(shp_list)) {
       index <- which.max(areas)
       
       return(rownames(sp_df_crp@data)[index])
-    }
-  })
-  r_val_s <-  pbsapply(1:ncell(r), function(i) {
-    
-    r_dupl <- r
-    r_dupl[i] <- 1
-    p <- rasterToPolygons(r_dupl) # Current cell -> polygon
-    sp_df_crp <- crop(sp_df_s, p)   # Crop initial polygons by current cell extent
-    
-    # Case 1: no polygon intersecting current cell
-    if (is.null(sp_df_crp)) {                   
       
-      return(NA)
-      
-      
-      # Case 2: one polygon intersecting current cell  
-    } else if (nrow(sp_df_crp@data) < 2) {     
-      
-      return(rownames(sp_df_crp@data)) 
-      
-      
-      # Case 3: multiple polygons intersecting current cell
-    } else {                                 
-      
-      areas <- gArea(sp_df_crp, byid = TRUE)
-      index <- which.max(areas)
-      
-      return(rownames(sp_df_crp@data)[index])
     }
   })
   
-  r_val_h = ifelse(is.na(r_val_h), NA, "1")
-  r_val_s = ifelse(is.na(r_val_s), NA, "2")
-  
-  r_val = data.frame(h = r_val_h, s = r_val_s)
-  r_val$hs <- NA
-  r_val$hs[r_val$h == 1 & r_val$s == 2] <- "1"
-  r_val$hs[r_val$h == 1 & is.na(r_val$s) == T] <- "1"
-  r_val$hs[is.na(r_val$h) == T & r_val$s == 2] <- "2"
-  
-  r_val = r_val$hs
+  r_val = ifelse(r_val %in% hard_i, "1", r_val)
+  r_val = ifelse(r_val %in% soft_i, "2", r_val)
+  r_val = ifelse(r_val %in% land_i, "3", r_val)
+  r_val = ifelse(r_val %in% ukwn_i, "4", r_val)
   
   # Write ID values covering the largest area per pixel into raster template
   r[] <- as.numeric(r_val)
   plot(r)
-  plot(sp_df, border = "grey45", add = TRUE)
+  plot(df, border = "grey45", add = TRUE)
   
   island_name = substr(shp_list[shp_i],1,nchar(shp_list[shp_i])-18)
   
