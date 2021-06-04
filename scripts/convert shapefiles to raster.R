e26d2f0ed39c4224a48c26e32763da82101bd952
Kisei Tanaka <1595661320@mil>
2021-06-04 01:04:28 -1000

    50 m

--- scripts/convert shapefiles to raster.R
+++ scripts/convert shapefiles to raster.R
@@ -27,7 +27,7 @@ for (shp_i in 1:length(shp_list)) {
   # Raster template 
   r <- raster(extent(df))
   projection(r) <- proj4string(df)
-  res(r) <- 500 # spatial resolution in m
+  res(r) <- 50 # spatial resolution in m
   
   # Per pixel, identify ID covering largest area, try jubilee.mcsapply() or pbsapply()
   r_val <-  jubilee.mcsapply(1:ncell(r), function(i) {
