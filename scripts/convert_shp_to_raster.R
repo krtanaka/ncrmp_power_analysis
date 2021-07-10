626af615d9217c6e26e7f5868b0093fc47b92a51
Kisei Tanaka <1595661320@mil>
2021-07-09 16:52:01 -1000

    annotation

--- scripts/convert_shp_to_raster.R
+++ scripts/convert_shp_to_raster.R
@@ -7,10 +7,16 @@ library(future.apply)
 
 rm(list = ls())
 
+### Instruction ###
+# 1. Get on pifsc.onaga.gov
+# 2. Use multicore and run 
+# 3. Get on Pifsc64, pull & push
+
 # jubilee.mcsapply() = works in Linux and MAC, faster
 # future_lapply)() = for Windows, needs plan(multisession) 
 
-# plan(multisession) 
+# plan(multisession) # Uncomment if you are running this on Windows OS
+
 
 ####################################
 ### Hard / Soft Bottom Substrate ###
@@ -33,7 +39,7 @@ for (shp_i in 1:length(shp_list)) {
   table = table %>% 
     group_by(HardSoft) %>% 
     summarise(i = paste0(i, collapse = ",")) 
-
+  
   # Raster template 
   r <- raster(extent(df))
   projection(r) <- proj4string(df)
@@ -80,9 +86,9 @@ for (shp_i in 1:length(shp_list)) {
   
   r = readAll(r)
   
-  raste_and_table = list(r, table)
+  raster_and_table = list(r, table)
   
-  save(raste_and_table, file = paste0("data/hardsoft_", island_name, ".RData"))
+  save(raster_and_table, file = paste0("data/hardsoft_", island_name, ".RData"))
   
   end = Sys.time()
   
@@ -164,7 +170,88 @@ for (shp_i in 1:length(shp_list)) {
   raster_and_table = list(r, table)
   
   save(raster_and_table, file = paste0("data/sector_", island_name, ".RData"))
+  
+  end = Sys.time()
+  
+  time = end - start
+  
+  print(paste0(island_name, "...done...took ", time, "..."))
+  
+}
 
+
+######################
+### reef component ###
+######################
+
+shp_list = list.files(path = "G:/GIS/reef/MHI/", pattern = ".shp"); shp_list
+shp_list = list.files(path = "/mnt/ldrive/ktanaka/GIS/reef/MHI/", pattern = ".shp"); shp_list
+
+for (shp_i in 1:length(shp_list)) {
+  
+  start = Sys.time()
+  
+  # shp_i = 6
+  
+  # Import shapefile
+  df <- readOGR(paste0("/mnt/ldrive/ktanaka/GIS/reef/MHI/", shp_list[shp_i]))[1]
+  df@data
+  table = data.frame(df@data, i = 0:(length(df)-1)); table
+  
+  table = table %>% 
+    group_by(REEF_ZONE) %>% 
+    summarise(i = paste0(i, collapse = ",")) 
+  
+  # Raster template 
+  r <- raster(extent(df))
+  projection(r) <- proj4string(df)
+  res(r) <- 100 # spatial resolution in m
+  
+  # Per pixel, identify ID covering largest area, try jubilee.mcsapply() or pbsapply(), or future_lapply()
+  # r_val <-  simplify2array(future_lapply(1:ncell(r), function(i) {
+  r_val <-  jubilee.mcsapply(1:ncell(r), mc.cores = 48, function(i) {
+    
+    r_dupl <- r
+    r_dupl[i] <- 1
+    p <- rasterToPolygons(r_dupl) # Current cell -> polygon
+    sp_df_crp <- crop(df, p)   # Crop initial polygons by current cell extent
+    
+    # Case 1: no polygon intersecting current cell
+    if (is.null(sp_df_crp)) {                   
+      
+      return(NA)
+      
+      
+      # Case 2: one polygon intersecting current cell  
+    } else if (nrow(sp_df_crp@data) < 2) {     
+      
+      return(rownames(sp_df_crp@data)) 
+      
+      
+      # Case 3: multiple polygons intersecting current cell
+    } else {                                 
+      
+      areas <- gArea(sp_df_crp, byid = TRUE)
+      index <- which.max(areas)
+      
+      return(rownames(sp_df_crp@data)[index])
+      
+    }
+  })
+  
+  # Write ID values covering the largest area per pixel into raster template
+  r[] <- as.numeric(r_val)
+  # plot(r, col = topo.colors(length(unique(r))))
+  # plot(df, border = "grey45", add = TRUE)
+  
+  island_name = substr(shp_list[shp_i], 1, nchar(shp_list[shp_i])-12)
+  
+  r = readAll(r)
+  
+  raster_and_table = list(r, table)
+  
+  save(raster_and_table, file = paste0("data/sector_", island_name, ".RData"))
+  
   end = Sys.time()
   
   time = end - start
