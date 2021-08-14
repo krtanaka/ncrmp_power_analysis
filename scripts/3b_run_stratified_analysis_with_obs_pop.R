b14b898581cbebd4ebf43044b8baa80e2d3f3160
Thomas Oliver <1505576515@mil>
2021-08-13 16:44:47 -1000

    Power analysis loop added

--- scripts/3b_run_stratified_analysis_with_obs_pop.R
+++ scripts/3b_run_stratified_analysis_with_obs_pop.R
@@ -18,7 +18,7 @@ load("data/modeled_survey_variability.RData") #modeled at grid scale
 # options(scipen = 999, digits = 2)
 
 # pick an island ----------------------------------------------------------
-island = c("Hawaii", "Kauai", "Lanai", "Maui", "Molokai", "Niihau", "Oahu" )[4]#[sample(1:7, 1)]
+island = c("Hawaii", "Kauai", "Lanai", "Maui", "Molokai", "Niihau", "Oahu" )[7]#[sample(1:7, 1)]
 print(island)
 
 # pick survey design ------------------------------------------------------
@@ -53,7 +53,7 @@ list = list.files(path = "outputs/", pattern = "_biomass"); list
 # adult or juvenile coral density
 # list = list.files(path = "outputs/", pattern = "_density"); list
 
-i = 4
+i = 5
 
 load(paste0("outputs/", list[i]))
 sp = strsplit(list[i], split = "_")[[1]][3]; sp
@@ -64,7 +64,9 @@ response_scale = strsplit(list[i], split = "_")[[1]][4]; response_scale
 ## Need to match sim and sdm
 sdm = sdm_output[,c("X", "Y", "longitude", "latitude", "year", "est" )]; rm(sdm_output)
 colnames(sdm)[1:2] = c("x", "y")
-sdm$est = exp(sdm$est)
+#est comes in as log-transformed biomass in g per m2, here we convert to grid-scale biomass in g
+# by exp function and multiplying by grid cell area in meters
+sdm$est = exp(sdm$est)*prod(res(survey_grid_kt))*1000^2
 
 # replace sim$years and sim$ages
 sim$years = sort(unique(sdm$year))
@@ -139,7 +141,7 @@ I
 
 load("data/survey_effort_MHI_2014-2019.RData")
 
-effort = c("high", "median", "low")[3]
+effort = c("high", "median", "low")[1]
 
 t_sample = survey_effort_MHI %>%
   subset(ISLAND == island) %>%
@@ -148,200 +150,223 @@ t_sample = survey_effort_MHI %>%
   as.numeric() %>%
   round(0)
 
-n_sims = 10 # number of simulations
-total_sample = t_sample # total sample efforts you want to deploy
-min_sets = 2 # minimum number of sets per strat
-# set_den = 2/1000 # number of sets per [grid unit = km] squared)
-trawl_dim = c(0.01, 0.0353) # 0.000353 sq.km (353 sq.m) from two 15-m diameter survey cylinders
-resample_cells = F
-
-n <- id <- division <- strat <- N <- NULL
-
-# sets <- sim_sets(sim,
-#                  resample_cells = resample_cells,
-#                  n_sims = n_sims,
-#                  trawl_dim = trawl_dim,
-#                  set_den = set_den,
-#                  min_sets = min_sets)
-
-strat_sets <- cell_sets <- NULL
-
-cells <- data.table(rasterToPoints(sim$grid))
-cells$sd = predict(g, cells); sd = cells[,c("strat", "sd")]; sd = sd %>% group_by(strat) %>% summarise(sd = mean(sd,na.rm=T)) # add modeled trophic biomass variability
-strat_det <- cells[, list(strat_cells = .N), by = "strat"]; strat_det
-strat_det$tow_area <- prod(trawl_dim); strat_det
-strat_det$cell_area <- prod(res(sim$grid)); strat_det
-strat_det$strat_area <- strat_det$strat_cells * prod(res(sim$grid)); strat_det
-strat_det = right_join(strat_det, sd); strat_det
-# strat_det$strat_sets <- round(strat_det$strat_area * set_den); strat_det
-strat_det$weight = strat_det$strat_area * strat_det$sd; strat_det
-strat_det$strat_sets = round((total_sample * strat_det$weight) / sum(strat_det$weight), 0); strat_det
-
-## Area vs Area*sd allocation
-# strat_det$strat_sets = round((total_sample * strat_det$strat_area) / sum(strat_det$strat_area), 0); strat_det
-strat_det$strat_sets[strat_det$strat_sets < min_sets] <- min_sets; strat_det # make sure minimum number of sets per strat is not 0 or 1
-
-cells <- merge(cells, strat_det, by = c("strat")) # add "strat" "strat_cells" "tow_area" ...
-
-i <- rep(seq(nrow(cells)), times = length(sim$years)) # number of cells * number of years
-y <- rep(sim$years, each = nrow(cells)) # number of years * number of cells
-
-cells <- cells[i, ] # increase the number of rows by number or years
-cells$year <- y
-
-i <- rep(seq(nrow(cells)), times = n_sims) # number of cells * number of simulations
-s <- rep(seq(n_sims), each = nrow(cells)) # number of simulations * number of cells
-
-cells <- cells[i, ] # increase the number of rows by number or simulations
-cells$sim <- s
-
-# .SD = "Subset of Data.table"
-# .N = number of instances
-# strat_sets, see unique(cells$strat_sets)
-
-sets <- cells[, .SD[sample(.N, strat_sets, replace = resample_cells)], 
-              by = c("sim", "year", "strat")]
-
-sets[, `:=`(cell_sets, .N), by = c("sim", "year", "cell")]
-sets$set <- seq(nrow(sets))
-sets
-
-setkeyv(sets, c("sim", "year", "cell"))
-
-sp_I <- data.table(sim$sp_N[, c("cell", "year", "N")])
-
-i <- rep(seq(nrow(sp_I)), times = n_sims)
-s <- rep(seq(n_sims), each = nrow(sp_I))
-
-sp_I <- sp_I[i, ]
-sp_I$sim <- s
-setdet <- merge(sets, sp_I, by = c("sim", "year", "cell"))
-
-setdet$n <- stats::rbinom(rep(1, nrow(setdet)), 
-                          size = round(setdet$N/setdet$cell_sets), 
-                          # prob = (setdet$tow_area/setdet$cell_area) * q(setdet$age))
-                          prob = (setdet$tow_area/setdet$cell_area))
-
-setdet$detection = ifelse(setdet$N > 0 & setdet$n == 0, 0, 1)
-setdet$detection = ifelse(setdet$N > 0 & setdet$n == 0, 0, 1)
-
-setkeyv(setdet, "set")
-setkeyv(sets, "set")
-
-sim$I <- I
-
-sim$setdet <- setdet
-
-setdet <- sim$setdet
-
-data = list(setdet = setdet)
-
-data$setdet <- data$setdet[, c("sim", 
-                               "year", 
-                               "division",
-                               "strat",
-                               "strat_area", 
-                               "tow_area",
-                               "set",
-                               "n"), 
+t_samples=seq(3,1000,length.out=12)#c(42,10,100,1000)*42#c(42,30,300,3000)#3,10,20,50,100,200,300,500,1000)
+df_power=data.frame(N=t_samples,RMSE=NA)
+for (i_sampling in 1:length(t_samples)){
+  t_sample=t_samples[i_sampling]
+  n_sims = 2 # number of simulations
+  total_sample = t_sample # total sample efforts you want to deploy
+  min_sets = 2 # minimum number of sets per strat
+  # set_den = 2/1000 # number of sets per [grid unit = km] squared)
+  trawl_dim = c(0.01, 0.0353) # 0.000353 sq.km (353 sq.m) from two 15-m diameter survey cylinders
+  resample_cells = F
+  
+  n <- id <- division <- strat <- N <- NULL
+  
+  # sets <- sim_sets(sim,
+  #                  resample_cells = resample_cells,
+  #                  n_sims = n_sims,
+  #                  trawl_dim = trawl_dim,
+  #                  set_den = set_den,
+  #                  min_sets = min_sets)
+  
+  strat_sets <- cell_sets <- NULL
+  
+  cells <- data.table(rasterToPoints(sim$grid))
+  cells$sd = predict(g, cells); sd = cells[,c("strat", "sd")]; sd = sd %>% group_by(strat) %>% summarise(sd = mean(sd,na.rm=T)) # add modeled trophic biomass variability
+  strat_det <- cells[, list(strat_cells = .N), by = "strat"]; strat_det
+  strat_det$tow_area <- prod(trawl_dim); strat_det
+  strat_det$cell_area <- prod(res(sim$grid)); strat_det
+  strat_det$strat_area <- strat_det$strat_cells * prod(res(sim$grid)); strat_det
+  strat_det = right_join(strat_det, sd); strat_det
+  #strat_det$strat_sets <- round(strat_det$strat_area * set_den); strat_det
+  
+  ## Area*sd allocation
+  #strat_det$weight = strat_det$strat_area * strat_det$sd; strat_det
+  #strat_det$strat_sets = round((total_sample * strat_det$weight) / sum(strat_det$weight), 0); strat_det
+  # 
+  ## Area  allocation
+  strat_det$strat_sets = round((total_sample * strat_det$strat_area) / sum(strat_det$strat_area), 0); strat_det
+  strat_det$strat_sets[strat_det$strat_sets < min_sets] <- min_sets; strat_det # make sure minimum number of sets per strat is not 0 or 1
+  
+  cells <- merge(cells, strat_det, by = c("strat")) # add "strat" "strat_cells" "tow_area" ...
+  
+  i <- rep(seq(nrow(cells)), times = length(sim$years)) # number of cells * number of years
+  y <- rep(sim$years, each = nrow(cells)) # number of years * number of cells
+  
+  cells <- cells[i, ] # increase the number of rows by number or years
+  cells$year <- y
+  
+  i <- rep(seq(nrow(cells)), times = n_sims) # number of cells * number of simulations
+  s <- rep(seq(n_sims), each = nrow(cells)) # number of simulations * number of cells
+  
+  cells <- cells[i, ] # increase the number of rows by number or simulations
+  cells$sim <- s
+  
+  # .SD = "Subset of Data.table"
+  # .N = number of instances
+  # strat_sets, see unique(cells$strat_sets)
+  
+  sets <- cells[, .SD[sample(.N, strat_sets, replace = resample_cells)], 
+                by = c("sim", "year", "strat")]
+  
+  sets[, `:=`(cell_sets, .N), by = c("sim", "year", "cell")]
+  sets$set <- seq(nrow(sets))
+  sets
+  
+  setkeyv(sets, c("sim", "year", "cell"))
+  
+  sp_I <- data.table(sim$sp_N[, c("cell", "year", "N")])
+  
+  i <- rep(seq(nrow(sp_I)), times = n_sims)
+  s <- rep(seq(n_sims), each = nrow(sp_I))
+  
+  sp_I <- sp_I[i, ]
+  sp_I$sim <- s
+  setdet <- merge(sets, sp_I, by = c("sim", "year", "cell"))
+  
+  setdet$n <- stats::rbinom(rep(1, nrow(setdet)), 
+                            size = round(setdet$N/setdet$cell_sets), 
+                            # prob = (setdet$tow_area/setdet$cell_area) * q(setdet$age))
+                            prob = .95)#(setdet$tow_area/setdet$cell_area))## DEBUGGGING !!!!!
+  
+  setdet$detection = ifelse(setdet$N > 0 & setdet$n == 0, 0, 1)
+  setdet$detection = ifelse(setdet$N > 0 & setdet$n == 0, 0, 1)
+  
+  setkeyv(setdet, "set")
+  setkeyv(sets, "set")
+  
+  sim$I <- I
+  
+  sim$setdet <- setdet
+  
+  setdet <- sim$setdet
+  
+  data = list(setdet = setdet)
+  
+  data$setdet <- data$setdet[, c("sim", 
+                                 "year", 
+                                 "division",
+                                 "strat",
+                                 "strat_area", 
+                                 "tow_area",
+                                 "set",
+                                 "n"), 
+                             with = FALSE]
+  data = data$setdet
+  data
+  
+  metric = "n"
+  strat_groups = c("sim", "year", "division", "strat", "strat_area", "tow_area")
+  survey_groups = c("sim", "year")
+  
+  confidence = 95
+  
+  Nh <- strat_area <- tow_area <- Wh <- total <- sumYh <- nh <- gh <- meanYh <- varYh <- meanYst_lcl <- meanYst <- varYst <- df <- meanYst_ucl <- sumYst <- N <- sumYst_lcl <- sumYst_ucl <- NULL
+  
+  lc <- (100 - confidence)/200; lc
+  uc <- (100 - confidence)/200 + (confidence/100); uc
+  
+  d <- copy(data)
+  d <- d[, c(strat_groups, metric), with = FALSE]
+  
+  setnames(d, names(d), c(strat_groups, "metric"))
+  setkeyv(d, strat_groups)
+  
+  strat_tab <- d[, list(sumYh = sum(metric), # sum of samples (n)
+                        meanYh = mean(metric, na.rm = T), # mean of samples (n)
+                        varYh = stats::var(metric, na.rm = T), # variance of samples (n)
+                        nh = .N), # number of strata
+                 by = strat_groups]; strat_tab
+  
+  strat_tab[, `:=`(Nh, strat_area/tow_area)]; strat_tab
+  strat_tab[, `:=`(Wh, Nh/sum(Nh)), by = survey_groups]; strat_tab
+  strat_tab[, `:=`(total, Nh * sumYh/nh)]; strat_tab
+  strat_tab[, `:=`(gh, Nh * (Nh - nh)/nh)]; strat_tab
+  
+  survey_tab <- strat_tab[, list(n = sum(nh, na.rm = T),
+                                 N = sum(Nh, na.rm = T), 
+                                 meanYst = sum(Wh * meanYh), 
+                                 varYst = (1/((sum(Nh))^2)) * sum(gh * varYh), 
+                                 df = ((sum(gh * varYh))^2)/(sum((gh^2 * varYh^2)/(nh - 1)))), by = survey_groups]; survey_tab
+  
+  survey_tab[, `:=`(meanYst_lcl, (meanYst - (sqrt(varYst)) * abs(stats::qt(lc, df))))]; survey_tab
+  survey_tab[, `:=`(meanYst_ucl, (meanYst + (sqrt(varYst)) * abs(stats::qt(lc, df))))]; survey_tab
+  survey_tab[, `:=`(sumYst, N * meanYst)]; survey_tab
+  survey_tab[, `:=`(sumYst_lcl, (sumYst - abs(stats::qt(lc, df)) * N * sqrt(varYst)))]; survey_tab
+  survey_tab[, `:=`(sumYst_ucl, (sumYst + abs(stats::qt(lc, df)) * N * sqrt(varYst)))]; survey_tab
+  
+  survey_tab[sapply(survey_tab, is.nan)] <- NA
+  
+  survey_tab <- survey_tab[, c(survey_groups,
+                               "n", 
+                               "N", 
+                               "df",
+                               "varYst", 
+                               "meanYst",
+                               "meanYst_lcl", 
+                               "meanYst_ucl",
+                               "sumYst",
+                               "sumYst_lcl", 
+                               "sumYst_ucl"), 
                            with = FALSE]
-data = data$setdet
-data
+  
+  survey_tab$varYst <- sqrt(survey_tab$varYst)
+  
+  setnames(survey_tab, names(survey_tab), c(survey_groups, 
+                                            "sets", 
+                                            "sampling_units", 
+                                            "df", 
+                                            "sd", 
+                                            "mean",
+                                            "mean_lcl",
+                                            "mean_ucl",
+                                            "total", 
+                                            "total_lcl", 
+                                            "total_ucl"))
+  survey_tab
+  
+  sim$total_strat = survey_tab
+  
+  total <- NULL
+  
+  I_hat <- sim$total_strat[, list(sim, year, total)]
+  names(I_hat) <- c("sim", "year", "I_hat")
+  
+  #I <- data.frame(year = sim$years, I = colSums(sim$I))
+  Iy <- data.frame(year = sim$years, I = colSums(sim$I))
+  
+  comp <- merge(I_hat, Iy, by = "year")
+  comp$error <- comp$I_hat - comp$I
+  means <- error_stats(comp$error)
+  sim$total_strat_error <- comp
+  sim$total_strat_error_stats <- means
+  I_hat <- sim$length_strat[, list(sim, year, length, total)]
+  
+  sim$total_strat_error_stats
+  sim$total_strat_error
+  df = sim$total_strat_error
+  
+  me = formatC(sim$total_strat_error_stats[1], digits = 3)
+  mae = formatC(sim$total_strat_error_stats[2], digits = 3)
+  mse = formatC(sim$total_strat_error_stats[3], digits = 3)
+  rmse = formatC(sim$total_strat_error_stats[4], digits = 3)
+  
+  df_power$RMSE[i_sampling]=rmse
+  df_power$TrueBiomass[i_sampling]=comp$I
+  df_power$SurveyBiomass[i_sampling]=comp$I_hat
+  print(i_sampling)
+}
 
-metric = "n"
-strat_groups = c("sim", "year", "division", "strat", "strat_area", "tow_area")
-survey_groups = c("sim", "year")
+ggplot(df_power,aes(N,RMSE))+geom_point()+geom_line()+scale_x_log10()
 
-confidence = 95
 
-Nh <- strat_area <- tow_area <- Wh <- total <- sumYh <- nh <- gh <- meanYh <- varYh <- meanYst_lcl <- meanYst <- varYst <- df <- meanYst_ucl <- sumYst <- N <- sumYst_lcl <- sumYst_ucl <- NULL
 
-lc <- (100 - confidence)/200; lc
-uc <- (100 - confidence)/200 + (confidence/100); uc
 
-d <- copy(data)
-d <- d[, c(strat_groups, metric), with = FALSE]
 
-setnames(d, names(d), c(strat_groups, "metric"))
-setkeyv(d, strat_groups)
 
-strat_tab <- d[, list(sumYh = sum(metric), # sum of samples (n)
-                      meanYh = mean(metric, na.rm = T), # mean of samples (n)
-                      varYh = stats::var(metric, na.rm = T), # variance of samples (n)
-                      nh = .N), # number of strata
-               by = strat_groups]; strat_tab
 
-strat_tab[, `:=`(Nh, strat_area/tow_area)]; strat_tab
-strat_tab[, `:=`(Wh, Nh/sum(Nh)), by = survey_groups]; strat_tab
-strat_tab[, `:=`(total, Nh * sumYh/nh)]; strat_tab
-strat_tab[, `:=`(gh, Nh * (Nh - nh)/nh)]; strat_tab
 
-survey_tab <- strat_tab[, list(n = sum(nh, na.rm = T),
-                               N = sum(Nh, na.rm = T), 
-                               meanYst = sum(Wh * meanYh), 
-                               varYst = (1/((sum(Nh))^2)) * sum(gh * varYh), 
-                               df = ((sum(gh * varYh))^2)/(sum((gh^2 * varYh^2)/(nh - 1)))), by = survey_groups]; survey_tab
-
-survey_tab[, `:=`(meanYst_lcl, (meanYst - (sqrt(varYst)) * abs(stats::qt(lc, df))))]; survey_tab
-survey_tab[, `:=`(meanYst_ucl, (meanYst + (sqrt(varYst)) * abs(stats::qt(lc, df))))]; survey_tab
-survey_tab[, `:=`(sumYst, N * meanYst)]; survey_tab
-survey_tab[, `:=`(sumYst_lcl, (sumYst - abs(stats::qt(lc, df)) * N * sqrt(varYst)))]; survey_tab
-survey_tab[, `:=`(sumYst_ucl, (sumYst + abs(stats::qt(lc, df)) * N * sqrt(varYst)))]; survey_tab
-
-survey_tab[sapply(survey_tab, is.nan)] <- NA
-
-survey_tab <- survey_tab[, c(survey_groups,
-                             "n", 
-                             "N", 
-                             "df",
-                             "varYst", 
-                             "meanYst",
-                             "meanYst_lcl", 
-                             "meanYst_ucl",
-                             "sumYst",
-                             "sumYst_lcl", 
-                             "sumYst_ucl"), 
-                         with = FALSE]
-
-survey_tab$varYst <- sqrt(survey_tab$varYst)
-
-setnames(survey_tab, names(survey_tab), c(survey_groups, 
-                                          "sets", 
-                                          "sampling_units", 
-                                          "df", 
-                                          "sd", 
-                                          "mean",
-                                          "mean_lcl",
-                                          "mean_ucl",
-                                          "total", 
-                                          "total_lcl", 
-                                          "total_ucl"))
-survey_tab
-
-sim$total_strat = survey_tab
-
-total <- NULL
-
-I_hat <- sim$total_strat[, list(sim, year, total)]
-names(I_hat) <- c("sim", "year", "I_hat")
-
-I <- data.frame(year = sim$years, I = colSums(sim$I))
-
-comp <- merge(I_hat, I, by = "year")
-comp$error <- comp$I_hat - comp$I
-means <- error_stats(comp$error)
-sim$total_strat_error <- comp
-sim$total_strat_error_stats <- means
-I_hat <- sim$length_strat[, list(sim, year, length, total)]
-
-sim$total_strat_error_stats
-sim$total_strat_error
-df = sim$total_strat_error
-
-me = formatC(sim$total_strat_error_stats[1], digits = 3)
-mae = formatC(sim$total_strat_error_stats[2], digits = 3)
-mse = formatC(sim$total_strat_error_stats[3], digits = 3)
-rmse = formatC(sim$total_strat_error_stats[4], digits = 3)
 
 label = paste0("ME = ", me, "\n", "MAE = ", mae, "\n", "MSE = ", mse, "\n", "RMSE = ", rmse)
 
