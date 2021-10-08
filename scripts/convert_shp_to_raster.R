ca38c55ee7d0dfee314e4211eeeeaa88f5f502df
krtanaka <kisei.tanaka@gmail.com>
2021-10-08 09:01:05 -1000

    ok

--- scripts/convert_shp_to_raster.R
+++ scripts/convert_shp_to_raster.R
@@ -17,11 +17,12 @@ rm(list = ls())
 
 # plan(multisession) # Uncomment if you are running this on Windows OS
 
-spatial_resolution = 50 # spatial resolution in m
+spatial_resolution = 1000 # spatial resolution in m
 cores = 64
 
 shp_path = "G:/GIS/"
 shp_path = "/mnt/ldrive/ktanaka/GIS/"
+shp_path = getwd()
 
 ##################################
 ### Hard/Soft Bottom Substrate ###
@@ -260,3 +261,82 @@ for (shp_i in 1:length(shp_list)) {
   print(paste0(island_name, "...done...took ", time, "..."))
   
 }
+
+############################
+### samoa marine reserve ###
+############################
+shp_list = list.files(path = paste0(shp_path, "/data/gis_reserve"), pattern = ".shp")[1]; shp_list
+for (shp_i in 1:length(shp_list)) {
+  
+  start = Sys.time()
+  
+  shp_i = 1
+  
+  # Import shapefile
+  df <- readOGR(paste0(shp_path, "/data/gis_reserve/", shp_list[shp_i]))
+  df <- df[df$Sector != "Land",]
+  df <- df[df$Sector != "Other",]
+
+  df@data
+  table = data.frame(df@data, i = 0:(length(df)-1)); table
+  
+  table = table %>% 
+    group_by(Sector) %>% 
+    summarise(i = paste0(i, collapse = ",")) 
+  
+  # Raster template 
+  r <- raster(extent(df))
+  projection(r) <- proj4string(df)
+  res(r) <- spatial_resolution # spatial resolution in m
+  
+  # Per pixel, identify ID covering largest area, try jubilee.mcsapply() or pbsapply(), or future_lapply()
+  # r_val <-  simplify2array(future_lapply(1:ncell(r), function(i) {
+  r_val <-  jubilee.mcsapply(1:ncell(r), mc.cores = cores, function(i) {
+    
+    r_dupl <- r
+    r_dupl[i] <- 1
+    p <- rasterToPolygons(r_dupl) # Current cell -> polygon
+    sp_df_crp <- crop(df, p)   # Crop initial polygons by current cell extent
+    
+    # Case 1: no polygon intersecting current cell
+    if (is.null(sp_df_crp)) {                   
+      
+      return(NA)
+      
+      # Case 2: one polygon intersecting current cell  
+    } else if (nrow(sp_df_crp@data) < 2) {     
+      
+      return(rownames(sp_df_crp@data)) 
+      
+      # Case 3: multiple polygons intersecting current cell
+    } else {                                 
+      
+      areas <- gArea(sp_df_crp, byid = TRUE)
+      index <- which.max(areas)
+      
+      return(rownames(sp_df_crp@data)[index])
+      
+    }
+  })
+  
+  # Write ID values covering the largest area per pixel into raster template
+  r[] <- as.numeric(r_val)
+  plot(r, col = topo.colors(length(unique(r))))
+  plot(df, border = "grey45", add = TRUE)
+  
+  island_name = tolower(substr(shp_list[shp_i], 1, nchar(shp_list[shp_i])-12))
+  
+  r = readAll(r)
+  
+  raster_and_table = list(r, table)
+  
+  save(raster_and_table, file = paste0("/mnt/ldrive/ktanaka/ncrmp_power_analysis/reef_", island_name, "_", spatial_resolution, ".RData"))
+  
+  end = Sys.time()
+  
+  time = end - start
+  
+  print(paste0(island_name, "...done...took ", time, "..."))
+  
+}
+
