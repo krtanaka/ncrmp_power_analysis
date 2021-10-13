1bded8dbde3638a3d9114b35fa3e290f0553cbb5
krtanaka <kisei.tanaka@gmail.com>
2021-10-12 14:00:40 -1000

    Update save_tif_bathymetry.R

--- scripts/save_tif_bathymetry.R
+++ scripts/save_tif_bathymetry.R
@@ -11,29 +11,24 @@ library(ggplot2)
 library(raster)
 library(tidyr)
 
-topo = raster("N:/GIS/Projects/CommonMaps/01_Preprocess/MARI/GUA/bathymetry/gua_mb_ld_5m_mosaic.tif")
+# topo = raster("N:/GIS/Projects/CommonMaps/01_Preprocess/MARI/GUA/bathymetry/gua_mb_ld_5m_mosaic.tif")
 topo = raster("N:/GIS/Projects/CommonMaps/01_Preprocess/MARI/GUA/bathymetry/gua_nthmp_dem_10m_mosaic.tif")
 
 topo[topo <= -30] <- NA
 topo[topo >= 0] <- NA
 
 topo = as.data.frame(rasterToPoints(topo)) %>% drop_na()
-# topo$Topography = ifelse(topo$gua_mb_ld_5m_mosaic %in% c(-30:0), topo$gua_mb_ld_5m_mosaic, NA)
-# topo = topo %>% drop_na() %>% dplyr::select(x, y, Topography)
 
 topo %>%
+  mutate(x = round(x*0.1, 0),
+         y = round(y*0.1, 0)) %>%
   group_by(x, y) %>%
-  summarise(x = mean(x),
-            y = mean(y),
-            Topography = mean(gua_nthmp_dem_10m_mosaic, na.rm = T)) %>%
-  ggplot(aes(x, y, fill = Topography, color = Topography)) +
-  # geom_hex(bins = 50)
-  # geom_tile(aes(width = 500, height = 500)) +
+  summarise(bathymetry = mean(bathymetry, na.rm = T)) %>%
+  ggplot(aes(x, y, fill = bathymetry)) +
   geom_raster() + 
-  # geom_point() + 
   scale_fill_viridis_c() +
-  coord_fixed() +
   ggdark::dark_theme_minimal() +
+  coord_fixed() +
   theme(axis.title = element_blank())
 
 save(topo, file = 'data/gis_bathymetry/raster/gua_nthmp_dem_10m_mosaic.RData')
