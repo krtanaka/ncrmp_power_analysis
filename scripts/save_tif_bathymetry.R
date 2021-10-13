64e603aa36d4312517e9435cccb43148feaddf8a
krtanaka <kisei.tanaka@gmail.com>
2021-10-12 17:16:23 -1000

    Update save_tif_bathymetry.R

--- scripts/save_tif_bathymetry.R
+++ scripts/save_tif_bathymetry.R
@@ -11,20 +11,23 @@ library(ggplot2)
 library(raster)
 library(tidyr)
 
-# topo = raster("N:/GIS/Projects/CommonMaps/01_Preprocess/MARI/GUA/bathymetry/gua_mb_ld_5m_mosaic.tif")
-topo = raster("N:/GIS/Projects/CommonMaps/01_Preprocess/MARI/GUA/bathymetry/gua_nthmp_dem_10m_mosaic.tif")
+topo = raster("L:/ktanaka/GIS/bathymetry/gua_nthmp_dem_10m_mosaic.tif") # Guam
+topo = raster("L:/ktanaka/GIS/bathymetry/sai_mb_5m.tif") #Saipan
+topo = raster("L:/ktanaka/GIS/bathymetry/Rota_5m_bathymetry.asc") 
+topo = raster("L:/ktanaka/GIS/bathymetry/tinian_5m.asc") 
 
 topo[topo <= -30] <- NA
 topo[topo >= 0] <- NA
 
 topo = as.data.frame(rasterToPoints(topo)) %>% drop_na()
+colnames(topo) = c("x", "y", "depth")
 
 topo %>%
   mutate(x = round(x*0.1, 0),
          y = round(y*0.1, 0)) %>%
   group_by(x, y) %>%
-  summarise(bathymetry = mean(bathymetry, na.rm = T)) %>%
-  ggplot(aes(x, y, fill = bathymetry)) +
+  summarise(depth = mean(depth, na.rm = T)) %>%
+  ggplot(aes(x, y, fill = depth)) +
   geom_raster() + 
   scale_fill_viridis_c() +
   ggdark::dark_theme_minimal() +
@@ -32,3 +35,6 @@ topo %>%
   theme(axis.title = element_blank())
 
 save(topo, file = 'data/gis_bathymetry/raster/gua_nthmp_dem_10m_mosaic.RData')
+save(topo, file = 'data/gis_bathymetry/raster/sai_mb_5m.tif.RData')
+save(topo, file = 'data/gis_bathymetry/raster/rota_5m_bathymetry.RData')
+
