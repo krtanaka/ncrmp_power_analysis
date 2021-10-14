c57a4ef14aedeb4616c93c8b99842fb47fe6659a
krtanaka <kisei.tanaka@gmail.com>
2021-10-13 19:47:43 -1000

    ok

--- scripts/save_tif_bathymetry.R
+++ scripts/save_tif_bathymetry.R
@@ -9,61 +9,27 @@ library(lattice)
 
 islands = c("gua", "rot", "sai", "tin")
 
-for (il in 1:length(islands)) {
+for (isl in 1:length(islands)) {
   
-  isl = 1
+  isl = 4
   
-  if (islands[isl] == "gua") {
-    
-    topo = raster("L:/ktanaka/GIS/bathymetry/gua_nthmp_dem_10m_mosaic.tif") # Guam
-    
-    topo[topo <= -30] <- NA
-    topo[topo >= 0] <- NA
-    
-    topo <- aggregate(topo, fact = 9.775)
-    res(topo)
-    
-  }
+  if (islands[isl] == "gua")  topo = raster("L:/ktanaka/GIS/bathymetry/gua_nthmp_dem_10m_mosaic.tif") # Guam
+  if (islands[isl] == "rot") topo = raster("L:/ktanaka/GIS/bathymetry/Rota_5m_bathymetry.asc") # Rota
+  if (islands[isl] == "sai") topo = raster("L:/ktanaka/GIS/bathymetry/sai_mb_5m.tif") # Saipan
+  if (islands[isl] == "tin") topo = raster("L:/ktanaka/GIS/bathymetry/tinian_5m.asc") # Tinian
   
-  if (islands[isl] == "rot") {
-    
-    topo = raster("L:/ktanaka/GIS/bathymetry/Rota_5m_bathymetry.asc") # Rota
-    
-    topo[topo <= -30] <- NA
-    topo[topo >= 0] <- NA
-    
-    topo <- aggregate(topo, fact = 20)
-    res(topo)
-    
-  }
+  topo[topo <= -30] <- NA
+  topo[topo >= 0] <- NA
   
-  if (islands[isl] == "sai") {
-    
-    topo = raster("L:/ktanaka/GIS/bathymetry/sai_mb_5m.tif") # Saipan
-    
-    topo[topo <= -30] <- NA
-    topo[topo >= 0] <- NA
-    
-    topo <- aggregate(topo, fact = 20)
-    res(topo)
-    
-  }
+  topo <- aggregate(topo, fact = 100/res(topo))
+  res(topo)
   
-  if (islands[isl] == "tin") {
-    
-    topo = raster("L:/ktanaka/GIS/bathymetry/tinian_5m.asc") # Tinian
-    
-    topo[topo <= -30] <- NA
-    topo[topo >= 0] <- NA
-    
-    topo <- aggregate(topo, fact = 20)
-    res(topo)
-    
-  }
+  topo = as.data.frame(rasterToPoints(topo)) %>% drop_na()
+  colnames(topo) = c("x", "y", "depth")
   
   topo %>%
     ggplot(aes(x, y, fill = depth)) +
-    geom_raster() + 
+    geom_raster() +
     scale_fill_viridis_c() +
     ggdark::dark_theme_minimal() +
     coord_fixed() +
@@ -71,9 +37,6 @@ for (il in 1:length(islands)) {
   
   save(topo, file = paste0('data/gis_bathymetry/raster/', islands[isl], '.RData'))
   
-  
 }
 
-wireframe(unclass(as.bathy(topo)), shade = TRUE, aspect = c(1/2, 0.1))
-
-
+wireframe(unclass(as.bathy(topo)), shade = T, aspect = c(1/2, 0.1))
