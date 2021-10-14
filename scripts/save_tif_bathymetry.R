8b81c1b80654c139d930834bc8c262bfd9af2c6e
krtanaka <kisei.tanaka@gmail.com>
2021-10-13 20:53:21 -1000

    Update save_tif_bathymetry.R

--- scripts/save_tif_bathymetry.R
+++ scripts/save_tif_bathymetry.R
@@ -11,7 +11,7 @@ islands = c("gua", "rot", "sai", "tin")
 
 for (isl in 1:length(islands)) {
   
-  isl = 4
+  isl = 2
   
   if (islands[isl] == "gua")  topo = raster("L:/ktanaka/GIS/bathymetry/gua_nthmp_dem_10m_mosaic.tif") # Guam
   if (islands[isl] == "rot") topo = raster("L:/ktanaka/GIS/bathymetry/Rota_5m_bathymetry.asc") # Rota
@@ -39,4 +39,34 @@ for (isl in 1:length(islands)) {
   
 }
 
-wireframe(unclass(as.bathy(topo)), shade = T, aspect = c(1/2, 0.1))
+load('data/gis_bathymetry/raster/gua.RData')
+
+wireframe(unclass(as.bathy(topo)), 
+          shade = T, 
+          aspect = c(1/2, 0.05),
+          par.box = c(col = "gray"),
+          scales = list(arrows = FALSE, col = "transparent"), # col="black" is required 
+          zlab = "", 
+          xlab = "",
+          ylab = "")
+
+topo = ggplot(topo) +
+  geom_tile(aes(x=x,y=y,fill=depth)) +
+  scale_fill_viridis() + 
+  coord_fixed() + 
+  theme_void()
+
+plot_gg(topo, 
+        multicore = T,
+        height = 5,
+        width = 6,
+        scale = 30, 
+        raytrace = TRUE,
+        # windowsize = c(1400, 866), 
+        zoom = 0.5, 
+        phi = 30, 
+        theta = 30)
+
+Sys.sleep(0.2)
+
+render_snapshot(clear = TRUE)
