ae6c1b4da4f36c5dd6b50d6cf161e0c21d5f3ee7
krtanaka <kisei.tanaka@gmail.com>
2021-10-14 21:13:17 -1000

    ok

--- scripts/save_SRTM15.R
+++ scripts/save_SRTM15.R
@@ -12,8 +12,13 @@ library(lattice)
 library(rayshader)
 
 topo = raster("data/gis_bathymetry/raster/srtm15.nc")
+topo = raster("data/gis_bathymetry/raster/srtm30.nc")
+topo = raster("data/gis_bathymetry/raster/etopo.nc")
+topo = raster("data/gis_bathymetry/raster/gebco.nc")
+
 topo = as.data.frame(rasterToPoints(topo))
-topo$Altitude = ifelse(topo$Altitude %in% c(-30:0), topo$Altitude, NA)
+topo$Altitude = ifelse(topo$Altitude %in% c(-3000:0), topo$Altitude, NA)
+topo$Altitude = ifelse(topo$Altitude < 0, topo$Altitude, NA)
 topo = topo %>% drop_na() %>% dplyr::select(x, y, Altitude)
 
 topo %>%
@@ -29,9 +34,30 @@ save(topo, file = 'data/gis_bathymetry/raster/Topography_SRTM15.RData')
 
 wireframe(unclass(as.bathy(topo)), 
           shade = T, 
-          aspect = c(1/2, 0.05),
+          aspect = c(1/0.3, 0.1),
           par.box = c(col = "gray"),
           scales = list(arrows = FALSE, col = "transparent"), # col="black" is required 
           zlab = "", 
           xlab = "",
           ylab = "")
+
+topo = ggplot(topo) +
+  geom_tile(aes(x=x,y=y,fill=depth)) +
+  scale_fill_viridis() + 
+  coord_fixed() + 
+  theme_void()
+
+plot_gg(topo, 
+        multicore = T,
+        height = 5,
+        width = 6,
+        scale = 30, 
+        raytrace = TRUE,
+        # windowsize = c(1400, 866), 
+        zoom = 0.5, 
+        phi = 30, 
+        theta = 30)
+
+Sys.sleep(0.2)
+
+render_snapshot(clear = TRUE)
